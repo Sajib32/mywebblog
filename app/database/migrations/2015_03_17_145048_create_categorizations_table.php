50472a84a460bea02d0130d26026b7f504c39e5d
Sajib32 <Sajibnstu@gmail.com>
2015-03-17 20:58:39 +0600

    Finish Migrations

--- app/database/migrations/2015_03_17_145048_create_categorizations_table.php
+++ app/database/migrations/2015_03_17_145048_create_categorizations_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriesPostsTable extends Migration {
+class CreateCategorizationsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,7 @@ class CreateCategoriesPostsTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('categories_posts', function($table){
+		Schema::create('categorizations', function($table){
 			$table->integer('category_id')->unsigned();
 			$table->foreign('category_id')->references('id')->on('categories');
 			$table->integer('post_id')->unsigned();
